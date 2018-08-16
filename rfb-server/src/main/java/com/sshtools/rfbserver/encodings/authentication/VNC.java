8b42ce296db9e26181de2ef4866a9dbd05831aff
Brett Smith <brett@hypersocket.com>
2018-08-16 09:56:32 +0100

    * Ignore JavaDoc errors (hopefully) * Added copyright * Added license template (GPL2 to GPL3)

--- rfb-server/src/main/java/com/sshtools/rfbserver/encodings/authentication/VNC.java
+++ rfb-server/src/main/java/com/sshtools/rfbserver/encodings/authentication/VNC.java
@@ -1,3 +1,22 @@
+/**
+ * RFB Server - Remote Frame Buffer (VNC Server) implementation. This is the base module if you want to create a VNC server. It takes a layered driver approach to add native specific features (which is recommened as the cross-platform default "Robot" driver is very slow).
+ *
+ * See the vncserver module for a concrete server implementation that has some native performance improvements for some platforms.
+ * Copyright © 2006 SSHTOOLS Limited (support@sshtools.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.sshtools.rfbserver.encodings.authentication;
 
 import java.io.DataInputStream;
